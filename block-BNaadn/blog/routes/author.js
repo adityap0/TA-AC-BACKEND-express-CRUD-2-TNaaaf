a1c81e34c4a360ac133e136c5eeb7b3c5278e065
Aditya Pathak <mynewchilly@gmail.com>
2021-07-16 10:01:37 +0530

    Re

---
+++ block-BNaadn/blog/routes/author.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+let Book = require("../models/book");
+let Author = require("../models/author");
+
+//READ
+
+module.exports = router;
